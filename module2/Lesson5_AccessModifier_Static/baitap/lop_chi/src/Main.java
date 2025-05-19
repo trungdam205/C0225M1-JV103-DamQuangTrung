dbd08108f7d0f1123f2b1f41e882aa8771626cff
trungdam205 <trungdam205@gmail.com>
2025-05-19 20:43:23 +0700

    up

---
+++ module2/Lesson5_AccessModifier_Static/baitap/lop_chi/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Alice");
+        s1.setClasses("C05");
+        System.out.println(s1);
+    }
+
+}
