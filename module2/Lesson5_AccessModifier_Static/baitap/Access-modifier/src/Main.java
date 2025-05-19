71686bd198c29b83a146439af745f2155fa9e84b
trungdam205 <trungdam205@gmail.com>
2025-05-19 20:31:27 +0700

    up

---
+++ module2/Lesson5_AccessModifier_Static/baitap/Access-modifier/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        System.out.println(c1.getArea());
+        System.out.println(c1.getRadius());
+    }
+}
